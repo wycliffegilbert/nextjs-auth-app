34e85cc8db3699dba7c20246729a9e166e2d4095
GerryNdungu <gerryjerry36@gmail.com>
2025-02-18 19:51:38 +0300

    Header changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
         {/* logo */}
         <Link href='/' className='text-2xl font-extrabold group cursor-pointer'>
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>
-            Auth
+            Soja
           </span>
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>
-            App
+            VMS
           </span>
         </Link>
         {/* add a navigation menu */}
